b74f9ccf5d452a8d8629afbc80fe822d35e97d74
Luan Reis <30740827+reisluan@users.noreply.github.com>
2023-06-15 19:31:22 -0300

    Delete test3-create.py

--- test3-create.py
+++
@@ -1,5 +0,0 @@
-https://drive.google.com/drive/folders/1wpV7seFX1Q4AiDOdRpHitWxU4NZA_MKy
-
-https://docs.google.com/document/d/1v6y-N6mEyyUDooE-qSW2HcABzk5Qn92t34cTQPvEqP8/edit
-
-https://techdocs.akamai.com/cps/docs/view-progress-certs#view-certificate-types
